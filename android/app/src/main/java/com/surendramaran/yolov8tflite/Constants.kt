9f7ec1ae78668f9a0b69008ec71e858e1f06a19a
Alexey Chentsov <1007leo@mail.ru>
2024-12-08 12:03:44 +0300

    Model updated to fix mixed up classes and added to the app

--- android/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++ android/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.surendramaran.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "yolo11n_float16.tflite"
-    val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
+    const val MODEL_PATH = "best_fixed_float32.tflite"
+    val LABELS_PATH: String? = "labels.txt" // provide your labels.txt file if the metadata not present in the model
 }
