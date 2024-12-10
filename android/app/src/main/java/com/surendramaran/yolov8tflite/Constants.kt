31b38253fac5aa0a8eb1a2bc88ab12acf386d218
Alexey Chentsov <1007leo@mail.ru>
2024-12-10 15:57:22 +0300

    Updated docker-compose, retrained model, removed unused files

--- android/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++ android/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.surendramaran.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "best_fixed_float32.tflite"
+    const val MODEL_PATH = "best_float32.tflite"
     val LABELS_PATH: String? = "labels.txt" // provide your labels.txt file if the metadata not present in the model
 }
